947a8bda5be3c23eb9d949b15ceccda051b1abad
Sangam Sushma <sushmasangam2022@gmail.com>
2026-06-10 00:14:08 +0530

    Create ai_engine.py

---
+++ ai_engine.py
@@ -0,0 +1,14 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+try:
+    models = client.models.list()
+    for m in models:
+        print(m.name)
+except Exception as e:
+    print("ERROR:", e)
